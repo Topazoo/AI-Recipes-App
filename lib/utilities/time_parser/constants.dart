70066517d299c4d4f8dfdb49bad0cfe0421527d0
Peter Swanson <topazoo42@gmail.com>
2023-05-27 21:36:55 -0700

    [Regex] Fix parsing of times

--- lib/utilities/time_parser/constants.dart
+++ lib/utilities/time_parser/constants.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: constant_identifier_names, non_constant_identifier_names
 
 class Constants {
-  static const String TIME_INTERVAL_PREFIX = "for|about";
-  static const String TIME_INTERVAL_SUFFIX = "seconds|minutes|hours|days|weeks|months|years|second|minute|hour|day|week|month|year";
-  static final RegExp TIME_INTERVAL_REGEX = RegExp('\b($TIME_INTERVAL_PREFIX)\b ([0-9]+(-[0-9]+)?) ($TIME_INTERVAL_SUFFIX)', caseSensitive: false);
+  static const String TIME_INTERVAL_PREFIX_GROUP = r"(for|about)";
+  static const String TIME_INTERVAL_SUFFIX_GROUP = r"(seconds|minutes|hours|days|weeks|months|years|second|minute|hour|day|week|month|year)";
+  static final RegExp TIME_INTERVAL_REGEX = RegExp(r'\b' + TIME_INTERVAL_PREFIX_GROUP + r'\b ([0-9]+(-[0-9]+)?) ' + TIME_INTERVAL_SUFFIX_GROUP, caseSensitive: false);
 }
